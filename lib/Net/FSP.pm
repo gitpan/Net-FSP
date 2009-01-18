30526d502919839959c34d29fdaa8ae5bacce0b8
Leon Timmermans <fawaka@gmail.com>
2009-01-18 01:31:13 +0000

    Import of LEONT/Net-FSP-0.14 from CPAN.
    gitpan-cpan-distribution: Net-FSP
    gitpan-cpan-version:      0.14
    gitpan-cpan-path:         LEONT/Net-FSP-0.14.tar.gz
    gitpan-cpan-author:       LEONT
    gitpan-cpan-maturity:     released

--- lib/Net/FSP.pm
+++ lib/Net/FSP.pm
@@ -2,7 +2,7 @@ package Net::FSP;
 use 5.006;
 use strict;
 use warnings;
-our $VERSION = 0.13;
+our $VERSION = 0.14;
 
 use Carp qw/croak/;
 use Socket qw/PF_INET SOCK_DGRAM sockaddr_in inet_aton INADDR_ANY/;
@@ -529,7 +529,7 @@ Net::FSP - A client implementation of the File Service Protocol
 
 =head1 VERSION
 
-This documentation refers to Net::FSP version 0.13
+This documentation refers to Net::FSP version 0.14
 
 =head1 SYNOPSIS
 
